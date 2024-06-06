1228f05915245e9cd651982d76d28dcf30bd0c55
BabaVo55 <ethemcode@outlook.com>
2024-06-06 13:53:40 +0100

    bug here stopped rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter} from 'react-router-dom';
+
+import {About, Contact, Experience, Feedbacks,
+Hero, Navbar, Tech, Works, StarCanvas} from 
+'./components';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      3D Developer Portfolio
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern 
+          bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
